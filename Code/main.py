16922cdf39dc461fba5bf53c14959e722698a346
ArnavSax <saxena.arnav27@gmail.com>
2020-10-10 17:54:29 -0700

    work in progress, adapting the knn code

--- Code/main.py
+++ Code/main.py
@@ -1,75 +1,119 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn import neighbors
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
+from matplotlib.colors import ListedColormap
 
-def read(filename):
-    titles=["accelX", "accelY", "accelZ", "magneX", "magneY", "magneZ", "gyrosX", "gyrosY", "gyrosZ", "time(ms)"]
-    dataframe = pd.read_csv(str(filename), names=titles).iloc[1:-1].astype(float)
-    dataframe["time(ms)"] -= dataframe.iloc[0, dataframe.columns.get_loc('time(ms)')] # scale time on the sensor down
-    return dataframe
+filename = "../Data/Right Arm Straight Reach/Fast Sampling Rate/default.log"
 
-#Mean average value
-def meanABS(d, col):
-    return d[col].abs().mean()
+data = pd.read_csv(str(filename), names=["accelX", "accelY", "accelZ", "magneX", "magneY", "magneZ", "gyrosX", "gyrosY", "gyrosZ", "time"]).iloc[1:-1].astype(float)
+data["time(ms)"] -= data.iloc[0, data.columns.get_loc('time(ms)')] # scale time on the sensor down
+
+# plt.plot(data["time"], data["accelZ"])
+# plt.xlabel('time')
+# plt.ylabel("Acceleration Z")
+# plt.show()
+
+# plt.plot(data["time"], data["accelY"])
+# plt.xlabel('time')
+# plt.ylabel("Acceleration Y")
+# plt.show()
+
+
+# plt.plot(data["time"], data["accelX"])
+# plt.xlabel('time')
+# plt.ylabel("Acceleration X")
+# plt.show()
+
+
+
+def genMatrix(col):
+    # feature extraction --> matrix
     
-#Root mean square
-def rootMS(d, col):
-    return np.sqrt(np.mean([i**2 for i in d[col]]))
 
-#Slow sign change
-def slopeSignChange(d, col):
-    times = []
-    data = d[col].diff().to_numpy()
-    for i in range(1, len(data)-1):
-        if data[i]*data[i+1] <= 0:
-            times.append(df.iloc[i]["time(ms)"])        
-    return times
+
+
+
+feature_matrix = np.stack([meanX,rmsX,sscX,posX,negX,crossX], axis=1) # Feature Matrix
+
+def knn(feature_matrix): # knn for arbitrary axis
+    df2 = pd.DataFrame( feature_matrix,
+                       columns=['Mean', 'RMS', 'SSC', 'POSX', 'NEG', 'CROSS']) # Label Added
+    print(df2)
+    rows_nbr = df2.shape[0]
     
-#Positive Peak
-def posPeak(d, col):
-    times = []
-    data = d[col].diff().to_numpy()
-    for i in range(1, len(data)-1):
-        if data[i]*data[i+1] < 0 and data[i] > data[i+1]:
-            times.append(df.iloc[i]["time(ms)"])
-    return times
+    X = np.array(df2.iloc[:rows_nbr, [0,4]])
+    x_rows, x_columns=X.shape
+    print(X)
+    Y = np.array(df2.iloc[:rows_nbr, 5])
+    #X = X.shape[1:]
+    print(X.shape)
+    print(Y.shape)
+    X_train, X_test, y_train, y_test = train_test_split(X,Y,test_size= 0.20)
+    
+    #oversampler=SMOTE(kind='regular',k_neighbors=2)
+    #make_pipeline_imb
+    h = .02
+    cmap_light = ListedColormap(['#FFAAAA', '#AAAAFF']) # colors for the KNN plot 
+    cmap_bold  = ListedColormap(['#FF0000', '#0000FF'])
+    def accuracy(k, X_train, y_train, X_test, y_test):
+        '''
+        compute accuracy of the classification based on k values 
+        '''
+        # instantiate learning model and fit data
+        knn = neighbors.KNeighborsClassifier(n_neighbors=k)    
+        knn.fit(X_train, y_train)
+        
+        # predict the response
+        pred = knn.predict(X_test)
+        
+        # evaluate and return  accuracy
+        return accuracy_score(y_test, pred)
+    for weights in ['uniform', 'distance']:
+            # we create an instance of Neighbours Classifier and fit the data.
+            clf = neighbors.KNeighborsClassifier(10, weights=weights)
+            clf.fit(X_train, y_train)
+        
+            # Plot the decision boundary. For that, we will assign a color to each
+            # point in the mesh [x_min, x_max]x[y_min, y_max].
+            x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+            y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+            xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                                 np.arange(y_min, y_max, h))
+            Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+        
+            # Put the result into a color plot
+            Z = Z.reshape(xx.shape)
+            fig = plt.figure()
+            plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+        
+            # Plot also the training points, x-axis = 'Glucose', y-axis = "BMI"
+            plt.scatter(X[:, 0], X[:, 1], c=Y, cmap=cmap_bold, edgecolor='k', s=20)   
+            plt.xlim(xx.min(), xx.max())
+            plt.ylim(yy.min(), yy.max())
+            plt.title("0/1 outcome classification (k = %i, weights = '%s')" % (5, weights))
+        
+            fig.savefig(weights +'.png')
+        
+            # evaluate
+            y_expected  = y_test
+            y_predicted = clf.predict(X_test)
+            
+            # print results
+    print('----------------------------------------------------------------------')
+    print('Classification report')
+    print('----------------------------------------------------------------------')
+    print('\n', classification_report(y_expected, y_predicted))
+    print('----------------------------------------------------------------------')
+    print('Accuracy = %5s' % round(accuracy(6, X_train, y_train, X_test, y_test), 3))
+    print('----------------------------------------------------------------------')
+    plt.show()
 
-#Negative Peak
-def negPeak(d, col):
-    times = []
-    data = d[col].diff().to_numpy()
-    for i in range(1, len(data)-1):
-        if data[i]*data[i+1] < 0 and data[i] < data[i+1]:
-            times.append(df.iloc[i]["time(ms)"])   
-    return times
 
-#Zero crossing
-def zeroCrossing(d, col):
-    times = []
-    data = d[col].to_numpy()
-    for i in range(0, len(data)-1):
-        if data[i]*data[i+1] < 0 or data[i] == 0:
-            times.append(df.iloc[i]["time(ms)"])
-    return times
+knn(feature_matrix)
 
-#Clip Dataframe
-def clip(d, num):
-    return d.iloc[num:-num]
-
-#chunk dataframe
-def chunk(d, window, overlap):
-    out = []
-    for i in range(0, d.size, window-overlap):
-        try:
-            out.append(d.iloc[i:i+window])
-        except:
-            out.append(d.iloc[i:])
-            break
-    return out
-
-df = read("right arm straight reach.log")
-
-plt.plot(df["time(ms)"], df["gyrosX"])
     
 
 
